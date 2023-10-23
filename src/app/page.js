0059d3fe8f8a987a30d192e241a458be42f0f89e
stixbunny <milenko.castillo@gmail.com>
2023-10-23 17:26:27 -0300

    prettier and testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,43 @@
-import Image from 'next/image'
+// 'use client';
+import Image from 'next/image';
+// import { useEffect, useState } from 'react';
+
+// export async function getServerSideProps() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+//   const data = await res.json();
+
+//   return {
+//     props: {
+//       todos: data,
+//     },
+//   };
+// }
 
 export default function Home() {
+  // const [todos, setTodos] = useState([]);
+  // useEffect(() => {
+  //   const fetchTodos = async () => {
+  //     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  //     const data = await res.json();
+  //     console.log(data)
+  //     setTodos(data);
+  //   };
+  //   fetchTodos();
+  // }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* {todos?.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        todos?.map(todo => (
+          <div key={todo.id}>
+            <p>
+              {todo.id}: {todo.title}
+            </p>
+          </div>
+        ))
+      )} */}
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -109,5 +144,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
